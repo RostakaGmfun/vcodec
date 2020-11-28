f8cd4159b59f692b5afbe66fa3ca444cd4970687
Rostyslav Kurylo <rostawesomegd@gmail.com>
2020-11-28 14:41:16 +0200

    Implement bitstream reader

--- include/vcodec/bitstream.h
+++ include/vcodec/bitstream.h
@@ -19,10 +19,11 @@ typedef struct vodec_bitstream_writer {
 
 typedef struct vodec_bitstream_reader {
     uint8_t buffer[VCODEC_BITSTREAM_READER_BUFFER_LEN];
-    uint32_t bytes_available;
+    uint32_t bits_available;
     uint32_t bit_pos;
     void *p_io_ctx;
     vcodec_read_t read;
+    vcodec_status_t last_status;
 } vcodec_bitstream_reader_t;
 
 /**
@@ -32,6 +33,9 @@ static vcodec_status_t vcodec_bitstream_writer_status(vcodec_bitstream_writer_t 
     return p_writer->last_status;
 }
 
+/**
+ * Discard buffered data and reset to initial state.
+ */
 static inline void vcodec_bitstream_writer_reset(vcodec_bitstream_writer_t *p_writer) {
     memset(p_writer->buffer, 0, sizeof(p_writer->buffer));
     p_writer->bit_pos = 0;
@@ -112,17 +116,67 @@ static inline void vcodec_bitstream_writer_write_exp_golomb(vcodec_bitstream_wri
 }
 
 /**
- * Get @c count bits into the LSBs of @c *p_out from a buffered bitstream.
- * @note 0 < count <= 32
+ * Refill buffer if needed.
  */
-static inline vcodec_status_t vcodec_bitstream_reader_getbits(vcodec_bitstream_reader_t *p_reader, uint32_t *p_out, uint32_t count) {
-
+static inline void vcodec_bitstream_reader_check_refill(vcodec_bitstream_reader_t *p_reader) {
+    if (p_reader->bit_pos == p_reader->bits_available) {
+        uint32_t bytes_read = 0;
+        p_reader->last_status = p_reader->read(p_reader->buffer, sizeof(p_reader->buffer), &bytes_read, p_reader->p_io_ctx);
+        p_reader->bits_available = bytes_read * 8;
+        p_reader->bit_pos = 0;
+    }
 }
 
 /**
  * Get @c count bits into the LSBs of @c *p_out from a buffered bitstream.
  * @note 0 < count <= 32
  */
-static inline vcodec_status_t vcodec_bitstream_reader_read_exp_golomb(vcodec_bitstream_reader_t *p_reader, uint32_t *p_out, uint32_t count) {
+static inline void vcodec_bitstream_reader_getbits(vcodec_bitstream_reader_t *p_reader, uint32_t *p_out, uint32_t count) {
+    *p_out = 0;
+    while (count > 0) {
+        vcodec_bitstream_reader_check_refill(p_reader);
+        const uint32_t to_read = MIN(count, 8 - p_reader->bit_pos % 8);
+        *p_out <<= to_read;
+        const uint32_t byte_offset = p_reader->bit_pos / 8;
+        const uint32_t bit_offset = (7 - p_reader->bit_pos % 8) - to_read + 1;
+        const uint8_t mask = ((1 << to_read) - 1) << bit_offset;
+        *p_out |= (p_reader->buffer[byte_offset] & mask) >> bit_offset;
+        count -= to_read;
+        p_reader->bit_pos += to_read;
+    }
+}
 
+static inline uint32_t vcodec_bitstream_reader_getzeroes(vcodec_bitstream_reader_t *p_reader) {
+    uint32_t ret = 0;
+    do {
+        vcodec_bitstream_reader_check_refill(p_reader);
+        const uint32_t to_read = 8 - p_reader->bit_pos % 8;
+        const uint32_t byte_offset = p_reader->bit_pos / 8;
+        const uint32_t bit_offset = (7 - p_reader->bit_pos % 8) - to_read + 1;
+        const uint8_t mask = ((1 << to_read) - 1) << bit_offset;
+        // For 32-bit word with highest `to_read` bits of data + 1 bit always set to use _builtin_clz even when all read bits are zero
+        // d...d   1   0...0
+        //   ^           ^
+        // to_read   31 - to_read
+        const uint8_t data = p_reader->buffer[byte_offset] & mask;
+        const uint32_t reg = (data << (32 - bit_offset - to_read)) | (1 << (31 - bit_offset - to_read));
+        const uint32_t num_zeroes = __builtin_clz(reg);
+        ret += num_zeroes;
+        p_reader->bit_pos += num_zeroes;
+        if (to_read != num_zeroes) {
+            break;
+        }
+    } while (1);
+    return ret;
+}
+
+/**
+ * Get @c count bits into the LSBs of @c *p_out from a buffered bitstream.
+ * @note 0 < count <= 32
+ */
+static inline uint32_t vcodec_bitstream_reader_read_exp_golomb(vcodec_bitstream_reader_t *p_reader) {
+    const uint32_t num_bits = vcodec_bitstream_reader_getzeroes(p_reader) + 1;
+    uint32_t ret;
+    vcodec_bitstream_reader_getbits(p_reader, &ret, num_bits);
+    return ret - 1;
 }
--- test/bitstream_test.c
+++ test/bitstream_test.c
@@ -6,17 +6,26 @@
 
 TEST_GROUP(bitstream_tests);
 
-#define TEST_IO_BUFFER_SIZE 32
+#define TEST_IO_BUFFER_SIZE (VCODEC_BITSTREAM_WRITER_BUFFER_LEN * 2)
 
 static struct {
     uint8_t buffer[TEST_IO_BUFFER_SIZE];
     uint32_t cursor;
 } io_ctx;
 
+_Static_assert(VCODEC_BITSTREAM_WRITER_BUFFER_LEN >= 4, "bitstream writer buffer too small for unit tests");
+
 static vcodec_status_t write_mock(const uint8_t *p_data, uint32_t size, void *ctx) {
     TEST_ASSERT_LESS_THAN(TEST_IO_BUFFER_SIZE + 1, io_ctx.cursor + size);
     memcpy(io_ctx.buffer + io_ctx.cursor, p_data, size);
     io_ctx.cursor += size;
+    return VCODEC_STATUS_OK;
+}
+
+static vcodec_status_t read_mock(uint8_t *p_data, uint32_t size, uint32_t *bytes_read, void *ctx) {
+    *bytes_read = MIN(size, sizeof(io_ctx.buffer) - io_ctx.cursor);
+    memcpy(p_data, io_ctx.buffer + io_ctx.cursor, *bytes_read);
+    io_ctx.cursor += *bytes_read;
     return VCODEC_STATUS_OK;
 }
 
@@ -28,71 +37,70 @@ TEST_TEAR_DOWN(bitstream_tests) {
 
 }
 
-TEST(bitstream_tests, test_bitstream_writer) {
+TEST(bitstream_tests, test_bitstream_writer_putbits) {
     vcodec_bitstream_writer_t writer = {
         .write = write_mock,
     };
-    _Static_assert(VCODEC_BITSTREAM_WRITER_BUFFER_LEN >= 4, "bitstream writer buffer too small for unit tests");
 
     vcodec_bitstream_writer_putbits(&writer, 0xabcd, 16);
-    TEST_ASSERT_EQUAL(0xab, writer.buffer[0]);
-    TEST_ASSERT_EQUAL(0xcd, writer.buffer[1]);
+    TEST_ASSERT_EQUAL_HEX(0xab, writer.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX(0xcd, writer.buffer[1]);
     vcodec_bitstream_writer_putbits(&writer, 0xa, 4);
     vcodec_bitstream_writer_putbits(&writer, 0xb, 4);
-    TEST_ASSERT_EQUAL(0xab, writer.buffer[2]);
+    TEST_ASSERT_EQUAL_HEX(0xab, writer.buffer[2]);
     vcodec_bitstream_writer_reset(&writer);
 
     vcodec_bitstream_writer_putbits(&writer, 1, 1);
     TEST_ASSERT_EQUAL(0, io_ctx.cursor);
-    TEST_ASSERT_EQUAL(0, io_ctx.buffer[io_ctx.cursor]);
+    TEST_ASSERT_EQUAL_HEX(0, io_ctx.buffer[io_ctx.cursor]);
     TEST_ASSERT_EQUAL(1, writer.bit_pos);
-    TEST_ASSERT_EQUAL(0x80, writer.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX(0x80, writer.buffer[0]);
 
     vcodec_bitstream_writer_putbits(&writer, 1, 7);
     TEST_ASSERT_EQUAL(0, io_ctx.cursor);
-    TEST_ASSERT_EQUAL(0, io_ctx.buffer[io_ctx.cursor]);
+    TEST_ASSERT_EQUAL_HEX(0, io_ctx.buffer[io_ctx.cursor]);
     TEST_ASSERT_EQUAL(8, writer.bit_pos);
-    TEST_ASSERT_EQUAL(0x81, writer.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX(0x81, writer.buffer[0]);
 
     vcodec_bitstream_writer_putbits(&writer, 0x1a, 5);
     TEST_ASSERT_EQUAL(0, io_ctx.cursor);
-    TEST_ASSERT_EQUAL(0, io_ctx.buffer[io_ctx.cursor]);
+    TEST_ASSERT_EQUAL_HEX(0, io_ctx.buffer[io_ctx.cursor]);
     TEST_ASSERT_EQUAL(8 + 5, writer.bit_pos);
-    TEST_ASSERT_EQUAL(0x1a << 3, writer.buffer[1]);
+    TEST_ASSERT_EQUAL_HEX(0x1a << 3, writer.buffer[1]);
 
     vcodec_bitstream_writer_putbits(&writer, 0x7ff, 11);
     TEST_ASSERT_EQUAL(0, io_ctx.cursor);
-    TEST_ASSERT_EQUAL(0, io_ctx.buffer[io_ctx.cursor]);
+    TEST_ASSERT_EQUAL_HEX(0, io_ctx.buffer[io_ctx.cursor]);
     TEST_ASSERT_EQUAL(24, writer.bit_pos);
     const uint8_t expected = (0x1a << 3) | (0x7ff >> 8);
-    TEST_ASSERT_EQUAL(expected, writer.buffer[1]);
-    TEST_ASSERT_EQUAL(0xff, writer.buffer[2]);
+    TEST_ASSERT_EQUAL_HEX(expected, writer.buffer[1]);
+    TEST_ASSERT_EQUAL_HEX(0xff, writer.buffer[2]);
 
     for (int i = 0; i < VCODEC_BITSTREAM_WRITER_BUFFER_LEN - 4; i++) {
         vcodec_bitstream_writer_putbits(&writer, 0xFF, 8);
         TEST_ASSERT_EQUAL(32 + i * 8, writer.bit_pos);
         TEST_ASSERT_EQUAL(0, io_ctx.cursor);
-        TEST_ASSERT_EQUAL(0xff, writer.buffer[2 + i]);
+        TEST_ASSERT_EQUAL_HEX(0xff, writer.buffer[2 + i]);
     }
 
     vcodec_bitstream_writer_putbits(&writer, 0xFF, 8);
     TEST_ASSERT_EQUAL(0, writer.bit_pos);
     TEST_ASSERT_EQUAL(VCODEC_BITSTREAM_WRITER_BUFFER_LEN, io_ctx.cursor);
-    TEST_ASSERT_EQUAL(0x81, io_ctx.buffer[0]);
-    TEST_ASSERT_EACH_EQUAL_UINT8(0, writer.buffer, sizeof(writer.buffer));
+    TEST_ASSERT_EQUAL_HEX(0x81, io_ctx.buffer[0]);
+    TEST_ASSERT_EACH_EQUAL_HEX8(0, writer.buffer, sizeof(writer.buffer));
 
     for (int i = 0; i < VCODEC_BITSTREAM_WRITER_BUFFER_LEN - 1; i++) {
         vcodec_bitstream_writer_putbits(&writer, 0x55, 8);
         TEST_ASSERT_EQUAL((i + 1) * 8, writer.bit_pos);
         TEST_ASSERT_EQUAL(VCODEC_BITSTREAM_WRITER_BUFFER_LEN, io_ctx.cursor);
-        TEST_ASSERT_EQUAL(0x55, writer.buffer[i]);
+        TEST_ASSERT_EQUAL_HEX(0x55, writer.buffer[i]);
     }
 
     vcodec_bitstream_writer_putbits(&writer, 0x55, 8);
     TEST_ASSERT_EQUAL(0, writer.bit_pos);
     TEST_ASSERT_EQUAL(2 * VCODEC_BITSTREAM_WRITER_BUFFER_LEN, io_ctx.cursor);
-    TEST_ASSERT_EACH_EQUAL_UINT8(0x55, io_ctx.buffer + VCODEC_BITSTREAM_WRITER_BUFFER_LEN, VCODEC_BITSTREAM_WRITER_BUFFER_LEN);
-    TEST_ASSERT_EACH_EQUAL_UINT8(0, writer.buffer, sizeof(writer.buffer));
+    TEST_ASSERT_EACH_EQUAL_HEX8(0x55, io_ctx.buffer + VCODEC_BITSTREAM_WRITER_BUFFER_LEN, VCODEC_BITSTREAM_WRITER_BUFFER_LEN);
+    TEST_ASSERT_EACH_EQUAL_HEX8(0, writer.buffer, sizeof(writer.buffer));
 }
 
 TEST(bitstream_tests, test_bitstream_writer_zeroes_ones_flush) {
@@ -101,66 +109,179 @@ TEST(bitstream_tests, test_bitstream_writer_zeroes_ones_flush) {
     };
     vcodec_bitstream_writer_putones(&writer, 10);
     TEST_ASSERT_EQUAL(0, io_ctx.cursor);
-    TEST_ASSERT_EQUAL(0, io_ctx.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX(0, io_ctx.buffer[0]);
     TEST_ASSERT_EQUAL(10, writer.bit_pos);
-    TEST_ASSERT_EQUAL(0xFF, writer.buffer[0]);
-    TEST_ASSERT_EQUAL(0xc0, writer.buffer[1]);
+    TEST_ASSERT_EQUAL_HEX(0xff, writer.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX(0xc0, writer.buffer[1]);
     vcodec_bitstream_writer_putzeroes(&writer, 6);
     TEST_ASSERT_EQUAL(0, io_ctx.cursor);
-    TEST_ASSERT_EQUAL(0, io_ctx.buffer[io_ctx.cursor]);
+    TEST_ASSERT_EQUAL_HEX(0, io_ctx.buffer[io_ctx.cursor]);
     TEST_ASSERT_EQUAL(16, writer.bit_pos);
-    TEST_ASSERT_EQUAL(0xc0, writer.buffer[1]);
+    TEST_ASSERT_EQUAL_HEX(0xc0, writer.buffer[1]);
 
     vcodec_bitstream_writer_putones(&writer, VCODEC_BITSTREAM_WRITER_BUFFER_LEN * 8 - 16);
     TEST_ASSERT_EQUAL(0, writer.bit_pos);
     TEST_ASSERT_EQUAL(VCODEC_BITSTREAM_WRITER_BUFFER_LEN, io_ctx.cursor);
-    TEST_ASSERT_EACH_EQUAL_UINT8(0xFF, io_ctx.buffer + 2, VCODEC_BITSTREAM_WRITER_BUFFER_LEN - 2);
-    TEST_ASSERT_EACH_EQUAL_UINT8(0, writer.buffer, sizeof(writer.buffer));
+    TEST_ASSERT_EACH_EQUAL_HEX8(0xFF, io_ctx.buffer + 2, VCODEC_BITSTREAM_WRITER_BUFFER_LEN - 2);
+    TEST_ASSERT_EACH_EQUAL_HEX8(0, writer.buffer, sizeof(writer.buffer));
 
     vcodec_bitstream_writer_putones(&writer, 1);
     vcodec_bitstream_writer_flush(&writer);
     TEST_ASSERT_EQUAL(VCODEC_BITSTREAM_WRITER_BUFFER_LEN + 1, io_ctx.cursor);
-    TEST_ASSERT_EQUAL(0x80, io_ctx.buffer[io_ctx.cursor - 1]);
+    TEST_ASSERT_EQUAL_HEX(0x80, io_ctx.buffer[io_ctx.cursor - 1]);
 }
 
-TEST(bitstream_tests, test_bitstream_writer_exp_golomb) {
+TEST(bitstream_tests, test_bitstream_writer_write_exp_golomb) {
     vcodec_bitstream_writer_t writer = {
         .write = write_mock,
     };
     vcodec_bitstream_writer_write_exp_golomb(&writer, 0);
     TEST_ASSERT_EQUAL(0, io_ctx.cursor);
-    TEST_ASSERT_EQUAL(0, io_ctx.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX(0, io_ctx.buffer[0]);
     TEST_ASSERT_EQUAL(1, writer.bit_pos);
-    TEST_ASSERT_EQUAL(0x80, writer.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX(0x80, writer.buffer[0]);
     vcodec_bitstream_writer_flush(&writer);
     TEST_ASSERT_EQUAL(1, io_ctx.cursor);
-    TEST_ASSERT_EQUAL(0x80, io_ctx.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX(0x80, io_ctx.buffer[0]);
     TEST_ASSERT_EQUAL(0, writer.bit_pos);
-    TEST_ASSERT_EQUAL(0x00, writer.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX(0x00, writer.buffer[0]);
 
     memset(&io_ctx, 0, sizeof(io_ctx));
     // 1 is coded as 0b010
     vcodec_bitstream_writer_write_exp_golomb(&writer, 1);
     TEST_ASSERT_EQUAL(0, io_ctx.cursor);
-    TEST_ASSERT_EQUAL(0, io_ctx.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX(0, io_ctx.buffer[0]);
     TEST_ASSERT_EQUAL(3, writer.bit_pos);
-    TEST_ASSERT_EQUAL(0x40, writer.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX(0x40, writer.buffer[0]);
     vcodec_bitstream_writer_flush(&writer);
 
     memset(&io_ctx, 0, sizeof(io_ctx));
     // 17 bits: 0b00000000100000001 -> 0x01, 0x01, 0x00
     vcodec_bitstream_writer_write_exp_golomb(&writer, 256);
     TEST_ASSERT_EQUAL(0, io_ctx.cursor);
-    TEST_ASSERT_EQUAL(0, io_ctx.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX(0, io_ctx.buffer[0]);
     TEST_ASSERT_EQUAL(17, writer.bit_pos);
-    TEST_ASSERT_EQUAL(0x00, writer.buffer[0]);
-    TEST_ASSERT_EQUAL(0x80, writer.buffer[1]);
-    TEST_ASSERT_EQUAL(0x80, writer.buffer[1]);
+    TEST_ASSERT_EQUAL_HEX(0x00, writer.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX(0x80, writer.buffer[1]);
+    TEST_ASSERT_EQUAL_HEX(0x80, writer.buffer[1]);
+}
+
+TEST(bitstream_tests, test_bitstream_reader_getbits) {
+    vcodec_bitstream_reader_t reader = {
+        .read = read_mock,
+    };
+    io_ctx.buffer[0] = 0x81;
+    io_ctx.buffer[1] = 0xff;
+    io_ctx.buffer[2] = 0xab;
+    io_ctx.buffer[3] = 0xcd;
+    uint32_t bits;
+    vcodec_bitstream_reader_getbits(&reader, &bits, 1);
+    TEST_ASSERT_EQUAL_HEX(1, bits);
+    TEST_ASSERT_EQUAL(VCODEC_BITSTREAM_READER_BUFFER_LEN, io_ctx.cursor);
+    vcodec_bitstream_reader_getbits(&reader, &bits, 7);
+    TEST_ASSERT_EQUAL_HEX(1, bits);
+    TEST_ASSERT_EQUAL(VCODEC_BITSTREAM_READER_BUFFER_LEN, io_ctx.cursor);
+    vcodec_bitstream_reader_getbits(&reader, &bits, 8);
+    TEST_ASSERT_EQUAL_HEX(0xff, bits);
+    TEST_ASSERT_EQUAL(VCODEC_BITSTREAM_READER_BUFFER_LEN, io_ctx.cursor);
+
+    vcodec_bitstream_reader_getbits(&reader, &bits, 16);
+    TEST_ASSERT_EQUAL_HEX(0xabcd, bits);
+    TEST_ASSERT_EQUAL(VCODEC_BITSTREAM_READER_BUFFER_LEN, io_ctx.cursor);
+}
+
+TEST(bitstream_tests, test_bitstream_reader_getzeroes) {
+    vcodec_bitstream_reader_t reader = {
+        .read = read_mock,
+    };
+    io_ctx.buffer[0] = 0x81;
+    io_ctx.buffer[1] = 0x00;
+    io_ctx.buffer[2] = 0x00;
+    io_ctx.buffer[3] = 0x80;
+    io_ctx.buffer[4] = 0x40;
+    uint32_t ret;
+    uint32_t bits;
+    ret = vcodec_bitstream_reader_getzeroes(&reader);
+    TEST_ASSERT_EQUAL(0, ret);
+    TEST_ASSERT_EQUAL(VCODEC_BITSTREAM_READER_BUFFER_LEN, io_ctx.cursor);
+    TEST_ASSERT_EQUAL(0, reader.bit_pos);
+    vcodec_bitstream_reader_getbits(&reader, &bits, 1);
+    TEST_ASSERT_EQUAL_HEX(1, bits);
+    TEST_ASSERT_EQUAL(1, reader.bit_pos);
+
+    ret = vcodec_bitstream_reader_getzeroes(&reader);
+    TEST_ASSERT_EQUAL(6, ret);
+    TEST_ASSERT_EQUAL(7, reader.bit_pos);
+    vcodec_bitstream_reader_getbits(&reader, &bits, 1);
+    TEST_ASSERT_EQUAL_HEX(1, bits);
+    TEST_ASSERT_EQUAL(8, reader.bit_pos);
+
+    ret = vcodec_bitstream_reader_getzeroes(&reader);
+    TEST_ASSERT_EQUAL(16, ret);
+    TEST_ASSERT_EQUAL(24, reader.bit_pos);
+    vcodec_bitstream_reader_getbits(&reader, &bits, 1);
+    TEST_ASSERT_EQUAL_HEX(1, bits);
+    TEST_ASSERT_EQUAL(25, reader.bit_pos);
+
+    ret = vcodec_bitstream_reader_getzeroes(&reader);
+    TEST_ASSERT_EQUAL(8, ret);
+    TEST_ASSERT_EQUAL(33, reader.bit_pos);
+}
+
+TEST(bitstream_tests, test_bitstream_reader_read_exp_golomb) {
+    vcodec_bitstream_reader_t reader = {
+        .read = read_mock,
+    };
+    // 0 as 0b1, 1 as 0b010, 256 as 0b00000000100000001, and 0 as 0b1
+    io_ctx.buffer[0] = 0xa0;
+    io_ctx.buffer[1] = 0x08;
+    io_ctx.buffer[2] = 0x0c;
+    uint32_t ret;
+    ret = vcodec_bitstream_reader_read_exp_golomb(&reader);
+    TEST_ASSERT_EQUAL(0, ret);
+    TEST_ASSERT_EQUAL(1, reader.bit_pos);
+
+    ret = vcodec_bitstream_reader_read_exp_golomb(&reader);
+    TEST_ASSERT_EQUAL(1, ret);
+    TEST_ASSERT_EQUAL(4, reader.bit_pos);
+
+    ret = vcodec_bitstream_reader_read_exp_golomb(&reader);
+    TEST_ASSERT_EQUAL(256, ret);
+    TEST_ASSERT_EQUAL(21, reader.bit_pos);
+
+    ret = vcodec_bitstream_reader_read_exp_golomb(&reader);
+    TEST_ASSERT_EQUAL(0, ret);
+    TEST_ASSERT_EQUAL(22, reader.bit_pos);
+}
+
+TEST(bitstream_tests, test_bitstream_exp_golomb_write_read) {
+    vcodec_bitstream_reader_t reader = {
+        .read = read_mock,
+    };
+
+    vcodec_bitstream_writer_t writer = {
+        .write = write_mock,
+    };
+
+    for (int i = 0; i < 16; i++) {
+        vcodec_bitstream_writer_write_exp_golomb(&writer, i);
+    }
+    vcodec_bitstream_writer_flush(&writer);
+    io_ctx.cursor = 0;
+    for (int i = 0; i < 16; i++) {
+        TEST_ASSERT_EQUAL(i, vcodec_bitstream_reader_read_exp_golomb(&reader));
+    }
 }
 
 TEST_GROUP_RUNNER(bitstream_tests)
 {
-    RUN_TEST_CASE(bitstream_tests, test_bitstream_writer);
+    RUN_TEST_CASE(bitstream_tests, test_bitstream_writer_putbits);
     RUN_TEST_CASE(bitstream_tests, test_bitstream_writer_zeroes_ones_flush);
-    RUN_TEST_CASE(bitstream_tests, test_bitstream_writer_exp_golomb);
+    RUN_TEST_CASE(bitstream_tests, test_bitstream_writer_write_exp_golomb);
+
+    RUN_TEST_CASE(bitstream_tests, test_bitstream_reader_getbits);
+    RUN_TEST_CASE(bitstream_tests, test_bitstream_reader_getzeroes);
+    RUN_TEST_CASE(bitstream_tests, test_bitstream_reader_read_exp_golomb);
+
+    RUN_TEST_CASE(bitstream_tests, test_bitstream_exp_golomb_write_read);
 }
